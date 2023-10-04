5b86126f5e1303c7cedd6a8dd0fe0cf0ee23a359
Jaden Yu <143239272+JadenJu@users.noreply.github.com>
2023-10-03 23:13:19 -0500

    Add files via upload

---
+++ Bored-Game/src/main/java/BackgrndCalcs/Dice.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BackgrndCalcs;
+
+/**
+ *
+ * @author jaden
+ */
+public class Dice {
+    private final int max_num;
+    private int dice_outcome;
+    
+    
+    Dice(){
+        max_num = 10;
+        dice_outcome = 0;
+    }
+    
+    public int rollDice()
+    {
+        //TODO find random number between 1-10 and send back to main board
+        return 0;
+    }
+}
+    
+    
+
---
+++ Bored-Game/src/main/java/Board/Board.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Board;
+
+/**
+ *
+ * @author jaden
+ */
+public class Board {
+    private final static Tiles[] map;
+    
+    
+}
