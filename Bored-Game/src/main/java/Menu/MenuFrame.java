889c51008ae8740d84d281c93eb5bed2ff805c05
James Stott <148901967+Jstott26@users.noreply.github.com>
2023-11-21 17:26:01 -0600

    Added options menu

--- Bored-Game/src/main/java/Menu/MenuFrame.java
+++ Bored-Game/src/main/java/Menu/MenuFrame.java
@@ -38,7 +38,20 @@ public class MenuFrame extends javax.swing.JFrame {
         tutorialPanel = new javax.swing.JPanel();
         tutorialTitle = new java.awt.Label();
         tutorialText = new java.awt.TextArea();
-        backButton = new javax.swing.JButton();
+        backButtonTutorial = new javax.swing.JButton();
+        optionsPanel = new javax.swing.JPanel();
+        backButtonOptions = new javax.swing.JButton();
+        optionsTitle = new javax.swing.JLabel();
+        controlsLabel = new javax.swing.JLabel();
+        diceContolLabel = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextPane1 = new javax.swing.JTextPane();
+        volumeControlLabel = new javax.swing.JLabel();
+        volumeSlider = new javax.swing.JSlider();
+        audioLabel = new javax.swing.JLabel();
+        graphicsLabel = new javax.swing.JLabel();
+        windowSizeLabel = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(153, 255, 255));
@@ -90,8 +103,8 @@ public class MenuFrame extends javax.swing.JFrame {
                     .addGroup(mainMenuLayout.createSequentialGroup()
                         .addGap(200, 200, 200)
                         .addGroup(mainMenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(quitButton, javax.swing.GroupLayout.DEFAULT_SIZE, 88, Short.MAX_VALUE)
-                            .addComponent(optionsButton, javax.swing.GroupLayout.DEFAULT_SIZE, 88, Short.MAX_VALUE)
+                            .addComponent(quitButton, javax.swing.GroupLayout.DEFAULT_SIZE, 72, Short.MAX_VALUE)
+                            .addComponent(optionsButton, javax.swing.GroupLayout.DEFAULT_SIZE, 72, Short.MAX_VALUE)
                             .addComponent(tutorialButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(playButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(204, 204, 204)))
@@ -121,10 +134,10 @@ public class MenuFrame extends javax.swing.JFrame {
 
         tutorialText.setFont(new java.awt.Font("Comic Sans MS", 0, 12)); // NOI18N
 
-        backButton.setText("Back");
-        backButton.addActionListener(new java.awt.event.ActionListener() {
+        backButtonTutorial.setText("Back");
+        backButtonTutorial.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                backButtonActionPerformed(evt);
+                backButtonTutorialActionPerformed(evt);
             }
         });
 
@@ -136,7 +149,7 @@ public class MenuFrame extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(tutorialPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(tutorialPanelLayout.createSequentialGroup()
-                        .addComponent(backButton, javax.swing.GroupLayout.DEFAULT_SIZE, 88, Short.MAX_VALUE)
+                        .addComponent(backButtonTutorial, javax.swing.GroupLayout.DEFAULT_SIZE, 88, Short.MAX_VALUE)
                         .addGap(402, 402, 402))
                     .addGroup(tutorialPanelLayout.createSequentialGroup()
                         .addComponent(tutorialTitle, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -151,9 +164,122 @@ public class MenuFrame extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(tutorialTitle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(15, 15, 15)
-                .addComponent(tutorialText, javax.swing.GroupLayout.DEFAULT_SIZE, 381, Short.MAX_VALUE)
+                .addComponent(tutorialText, javax.swing.GroupLayout.DEFAULT_SIZE, 374, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(backButton)
+                .addComponent(backButtonTutorial)
+                .addContainerGap())
+        );
+
+        optionsPanel.setEnabled(false);
+        optionsPanel.setPreferredSize(new java.awt.Dimension(500, 500));
+
+        backButtonOptions.setText("Back");
+        backButtonOptions.setToolTipText("");
+        backButtonOptions.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                backButtonOptionsActionPerformed(evt);
+            }
+        });
+
+        optionsTitle.setFont(new java.awt.Font("Comic Sans MS", 1, 36)); // NOI18N
+        optionsTitle.setText("Options");
+
+        controlsLabel.setFont(new java.awt.Font("Comic Sans MS", 1, 24)); // NOI18N
+        controlsLabel.setText("Controls");
+
+        diceContolLabel.setFont(new java.awt.Font("Comic Sans MS", 0, 18)); // NOI18N
+        diceContolLabel.setText("Roll Dice");
+
+        jTextPane1.setFont(new java.awt.Font("Comic Sans MS", 1, 14)); // NOI18N
+        jTextPane1.setText("E");
+        jTextPane1.setAutoscrolls(false);
+        jTextPane1.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
+        jTextPane1.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
+            public void propertyChange(java.beans.PropertyChangeEvent evt) {
+                jTextPane1PropertyChange(evt);
+            }
+        });
+        jScrollPane1.setViewportView(jTextPane1);
+
+        volumeControlLabel.setFont(new java.awt.Font("Comic Sans MS", 0, 18)); // NOI18N
+        volumeControlLabel.setText("Volume");
+
+        volumeSlider.setMajorTickSpacing(10);
+        volumeSlider.setMinorTickSpacing(1);
+        volumeSlider.setSnapToTicks(true);
+        volumeSlider.setValue(0);
+        volumeSlider.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+
+        audioLabel.setFont(new java.awt.Font("Comic Sans MS", 1, 24)); // NOI18N
+        audioLabel.setText("Audio");
+
+        graphicsLabel.setFont(new java.awt.Font("Comic Sans MS", 1, 24)); // NOI18N
+        graphicsLabel.setText("Graphics");
+
+        windowSizeLabel.setFont(new java.awt.Font("Comic Sans MS", 0, 18)); // NOI18N
+        windowSizeLabel.setText("Window Size");
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "500 x 500", "800 x 600", "1024 x 768", "1920 x 1080" }));
+
+        javax.swing.GroupLayout optionsPanelLayout = new javax.swing.GroupLayout(optionsPanel);
+        optionsPanel.setLayout(optionsPanelLayout);
+        optionsPanelLayout.setHorizontalGroup(
+            optionsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(optionsPanelLayout.createSequentialGroup()
+                .addGroup(optionsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(optionsPanelLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(optionsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(backButtonOptions, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(optionsTitle, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, optionsPanelLayout.createSequentialGroup()
+                                .addGap(12, 12, 12)
+                                .addGroup(optionsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(audioLabel)
+                                    .addGroup(optionsPanelLayout.createSequentialGroup()
+                                        .addGroup(optionsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                            .addComponent(controlsLabel)
+                                            .addComponent(diceContolLabel)
+                                            .addGroup(optionsPanelLayout.createSequentialGroup()
+                                                .addComponent(volumeControlLabel)
+                                                .addGap(11, 11, 11)))
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addGroup(optionsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(volumeSlider, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                    .addComponent(graphicsLabel)))))
+                    .addGroup(optionsPanelLayout.createSequentialGroup()
+                        .addGap(52, 52, 52)
+                        .addComponent(windowSizeLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(162, Short.MAX_VALUE))
+        );
+        optionsPanelLayout.setVerticalGroup(
+            optionsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, optionsPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(optionsTitle)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(controlsLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(optionsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane1)
+                    .addComponent(diceContolLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(audioLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(optionsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(volumeControlLabel)
+                    .addComponent(volumeSlider, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(graphicsLabel)
+                .addGap(18, 18, 18)
+                .addGroup(optionsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jComboBox1)
+                    .addComponent(windowSizeLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 139, Short.MAX_VALUE)
+                .addComponent(backButtonOptions)
                 .addContainerGap())
         );
 
@@ -163,13 +289,18 @@ public class MenuFrame extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(mainMenu, javax.swing.GroupLayout.DEFAULT_SIZE, 503, Short.MAX_VALUE)
+                .addComponent(mainMenu, javax.swing.GroupLayout.DEFAULT_SIZE, 511, Short.MAX_VALUE)
                 .addGap(0, 0, Short.MAX_VALUE))
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                     .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(tutorialPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addContainerGap()))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(optionsPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 508, Short.MAX_VALUE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -179,23 +310,28 @@ public class MenuFrame extends javax.swing.JFrame {
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
                     .addGap(10, 10, 10)
-                    .addComponent(tutorialPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 501, Short.MAX_VALUE)
+                    .addComponent(tutorialPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 502, Short.MAX_VALUE)
                     .addContainerGap(278, Short.MAX_VALUE)))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(optionsPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 661, Short.MAX_VALUE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void backButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backButtonActionPerformed
+    private void backButtonTutorialActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backButtonTutorialActionPerformed
         changePanel(mainMenu);
-    }//GEN-LAST:event_backButtonActionPerformed
+    }//GEN-LAST:event_backButtonTutorialActionPerformed
 
     private void quitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_quitButtonActionPerformed
         System.exit(0);
     }//GEN-LAST:event_quitButtonActionPerformed
 
     private void optionsButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_optionsButtonActionPerformed
-        // TODO add your handling code here:
+        changePanel(optionsPanel);
     }//GEN-LAST:event_optionsButtonActionPerformed
 
     private void tutorialButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tutorialButtonActionPerformed
@@ -205,6 +341,14 @@ public class MenuFrame extends javax.swing.JFrame {
     private void playButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_playButtonActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_playButtonActionPerformed
+
+    private void backButtonOptionsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backButtonOptionsActionPerformed
+        changePanel(mainMenu);
+    }//GEN-LAST:event_backButtonOptionsActionPerformed
+
+    private void jTextPane1PropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_jTextPane1PropertyChange
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextPane1PropertyChange
     
     private void changePanel(JPanel panel) { // Code from https://stackoverflow.com/questions/5077321/how-could-i-make-the-jframe-content-change-to-corresponding-click
     getContentPane().removeAll();
@@ -249,9 +393,19 @@ public class MenuFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton backButton;
+    private javax.swing.JLabel audioLabel;
+    private javax.swing.JButton backButtonOptions;
+    private javax.swing.JButton backButtonTutorial;
+    private javax.swing.JLabel controlsLabel;
+    private javax.swing.JLabel diceContolLabel;
+    private javax.swing.JLabel graphicsLabel;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextPane jTextPane1;
     private javax.swing.JPanel mainMenu;
     private javax.swing.JButton optionsButton;
+    private javax.swing.JPanel optionsPanel;
+    private javax.swing.JLabel optionsTitle;
     private javax.swing.JButton playButton;
     private javax.swing.JButton quitButton;
     private javax.swing.JLabel title;
@@ -259,5 +413,8 @@ public class MenuFrame extends javax.swing.JFrame {
     private javax.swing.JPanel tutorialPanel;
     private java.awt.TextArea tutorialText;
     private java.awt.Label tutorialTitle;
+    private javax.swing.JLabel volumeControlLabel;
+    private javax.swing.JSlider volumeSlider;
+    private javax.swing.JLabel windowSizeLabel;
     // End of variables declaration//GEN-END:variables
 }
